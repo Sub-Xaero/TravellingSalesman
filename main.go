440b58a3f66d1d8ca128cbdd4c767f686dbaf9f9
SubXaero <dylcla@me.com>
2017-10-08 16:23:07 +0100

    Add storage type for concept of cities and distances

--- main.go
+++ main.go
@@ -5,6 +5,22 @@ import (
 	"time"
 )
 
+
+type Distance struct {
+	City1, City2 int
+	Distance     int
+}
+
+const (
+	_       = iota
+	London  = iota
+	Bath    = iota
+	Bristol = iota
+	Leeds   = iota
+	Glasgow = iota
+	Swansea = iota
+)
+
 func main() {
 	rand.Seed(time.Now().Unix())
 }

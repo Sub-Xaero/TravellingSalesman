1bf993f38e43ac17fde8167f45e27a543d1756d6
SubXaero <dylcla@me.com>
2017-10-08 16:24:41 +0100

    Add preliminary, arbritrary distances between cities

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
+	"strconv"
+	"subxaero/ga"
 	"time"
 )
 
@@ -23,4 +26,29 @@ const (
 
 func main() {
 	rand.Seed(time.Now().Unix())
+
+	distances := []Distance{
+		{Bristol, Bath, 14},
+		{Bristol, London, 105},
+		{Bristol, Leeds, 275},
+		{London, Bath, 110},
+		{London, Leeds, 227},
+		{Bath, Leeds, 291},
+		{Glasgow, London, 724},
+		{Glasgow, Bath, 850},
+		{Glasgow, Bristol, 802},
+		{Glasgow, Leeds, 620},
+
+		{Swansea, London, 250},
+		{Swansea, Bath, 105},
+		{Swansea, Bristol, 98},
+		{Swansea, Leeds, 324},
+		{Swansea, Glasgow, 821},
+	}
+	maxDistance := 0
+	for _, i := range distances {
+		maxDistance += i.Distance
+	}
+	fmt.Println(distances)
+
 }

0f1ccad46d437e401e3941738ba5a451f6979649
SubXaero <dylcla@me.com>
2017-10-09 22:56:40 +0100

    Double digit bitstring, pad 0s, change all encoding/decoding

--- main.go
+++ main.go
@@ -80,37 +80,44 @@ func main() {
 		bitstring := ""
 		for i := 0; i < length; i++ {
 			choice := rand.Int() % len(possibilities)
+			if possibilities[choice] < 10 {
+				bitstring += "0"
+			}
 			bitstring += strconv.Itoa(possibilities[choice])
 			possibilities = append(possibilities[:choice], possibilities[choice+1:]...)
 		}
-		bitstring += string(bitstring[0])
+		bitstring += string(bitstring[0:2])
 		return bitstring
 	})
 
 	ga.SetCrossoverFunc(func(gene, spouse ga.Genome) []ga.Genome {
-
 		return []ga.Genome{gene, spouse}
 	})
 
 	ga.SetMutateFunc(func(gene ga.Genome, chance int) ga.Genome {
 		sequence := []rune(gene.Sequence)
-		choice1 := (rand.Int() % (len(sequence) - 2)) + 1 // Start and end city must not change, so swap between
-		choice2 := (rand.Int() % (len(sequence) - 2)) + 1 //
+		newSequence := []rune(gene.Sequence)
+		min := 2
+		max := len(sequence) - 2
 
-		sequence[choice1], sequence[choice2] = sequence[choice2], sequence[choice1]
-		//fmt.Println(string(sequence))
-		return ga.Genome{string(sequence)}
+		choice1 := 2 * ((rand.Int()%(max-min) + min) / 2)
+		choice2 := 2 * ((rand.Int()%(max-min) + min) / 2)
+
+		newSequence[choice1], newSequence[choice1+1] = sequence[choice2], sequence[choice2+1]
+		newSequence[choice2], newSequence[choice2+1] = sequence[choice1], sequence[choice1+1]
+		return ga.Genome{Sequence: string(newSequence)}
 	})
+
+	gene := ga.Genome{Sequence: ga.GenerateBitString(len(distances))}
+	fmt.Println(gene.Sequence)
+	fmt.Println(gene.Mutate(len(gene.Sequence)).Sequence)
 
 	ga.SetFitnessFunc(func(gene ga.Genome) int {
 		genomeSequence := gene.Sequence
 		totalDistance := 0
-		for index := range genomeSequence {
-			if index == len(genomeSequence)-1 {
-				break
-			}
-			city1, _ := strconv.Atoi(string(genomeSequence[index]))
-			city2, _ := strconv.Atoi(string(genomeSequence[index+1]))
+		for i := 0; i < len(genomeSequence)-4; i += 4 {
+			city1, _ := strconv.Atoi(string(genomeSequence[i : i+2]))
+			city2, _ := strconv.Atoi(string(genomeSequence[i+2 : i+3]))
 
 			totalDistance += distances[city1][city2]
 		}
